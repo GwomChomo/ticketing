5676dcdf2da8555d0e4ae6286c1ba7d274429d8e
cgwom <chomo.gwom@camcog.com>
2023-08-12 19:13:05 +0200

    added mongo db deployment and jwt tokens for signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from "../errors/request-validation-error";
 import {BadRequestError} from "../errors/bad-request-error";
@@ -23,6 +24,15 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+    //generate json web toke and store it on the session object
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email,
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt,
+    }
 
     res.status(201).send(user);
 });
